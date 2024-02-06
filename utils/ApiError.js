54173800f419e80c6b2fb1b7175c3e05ac958263
FaizanPotrick <faizan.potrick02@gmail.com>
2024-02-06 12:09:49 +0530

    Update utils

--- utils/ApiError.js
+++ utils/ApiError.js
@@ -1,9 +1,10 @@
 class ApiError extends Error {
-  constructor(message = "Something went wrong", statusCode, name) {
+  constructor(message = "Something went wrong", statusCode, name,  errors = [], stack="") {
     super(message);
     this.statusCode = statusCode;
     this.message = message;
     this.name = name || "ApiError";
+    this.errors = errors;
     this.success = false;
 
     if (stack) {
--- utils/ApiResponse.js
+++ utils/ApiResponse.js
@@ -1,5 +1,5 @@
 class ApiResponse {
-  constructor(statusCode, data, message = "Success") {
+  constructor(data, message = "Success", statusCode) {
     this.statusCode = statusCode;
     this.data = data;
     this.message = message;
