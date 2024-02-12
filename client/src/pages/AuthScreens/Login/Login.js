ba2998ada10fde9cc90d2fa3a547d388226c488f
FaizanPotrick <faizan.potrick02@gmail.com>
2024-02-12 08:53:36 +0530

    add login form

--- client/src/pages/AuthScreens/Login/Login.js
+++ client/src/pages/AuthScreens/Login/Login.js
@@ -1,28 +1,80 @@
 import React, { useState, useContext } from "react";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
-import ButtonGroup from "@mui/material/ButtonGroup";
-import { Login as LoginFunction } from "../../../utils/Index";
 import { StateContext } from "../../../context/StateContext";
+import { Login as LoginFunction } from "../../../utils/Index";
+
+const initialState = {
+  username: "",
+  password: "",
+  usernameError: false,
+  passwordError: false,
+};
 
 const Login = () => {
-  const { setToken } = useContext(StateContext);
+  const { setToken, toggleLoading } = useContext(StateContext);
 
-  const [login, setLogin] = useState({
-    username: "csi",
-    password: "password",
-    usernameError: false,
-    passwordError: false,
-  });
+  const [login, setLogin] = useState(initialState);
+
+  const handleInputChange = (field, value) => {
+    setLogin({
+      ...login,
+      [field]: value,
+      [`${field}Error`]: false,
+    });
+  };
+
   return (
     <div>
-      <ButtonGroup
-        variant="contained"
-        aria-label="outlined primary button group"
-      >
-        <Button onClick={() => LoginFunction(login, setLogin, setToken)}>
+      <div className="d-flex align-items-center">
+        <h2>Login</h2>
+      </div>
+      <div className="container-fluid my-3">
+        <Box component="form" noValidate autoComplete="off">
+          <div className="row gy-4">
+            <div className="col-md-6 col-lg-8 d-flex align-items-end">
+              <TextField
+                style={{ width: "100%" }}
+                id="standard-basic"
+                label="Username"
+                variant="standard"
+                value={login.username}
+                onChange={(e) => handleInputChange("username", e.target.value)}
+                error={login.usernameError}
+                helperText={
+                  login.usernameError ? "Username cannot be empty" : ""
+                }
+              />
+            </div>
+            <div className="col-md-6 col-lg-8 d-flex align-items-end">
+              <TextField
+                style={{ width: "100%" }}
+                id="standard-basic"
+                label="Password"
+                variant="standard"
+                value={login.password}
+                onChange={(e) => handleInputChange("password", e.target.value)}
+                error={login.passwordError}
+                helperText={
+                  login.passwordError ? "Password cannot be empty" : ""
+                }
+              />
+            </div>
+          </div>
+        </Box>
+      </div>
+      <div className="mt-5 w-100 d-flex align-items-center justify-content-center">
+        <Button
+          variant="contained"
+          style={{ borderRadius: "12px", padding: "10px 50px" }}
+          onClick={() =>
+            LoginFunction(login, setLogin, toggleLoading, setToken, initialState)
+          }
+        >
           Login
         </Button>
-      </ButtonGroup>
+      </div>
     </div>
   );
 };
--- client/src/utils/Index.js
+++ client/src/utils/Index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const Login = async (login, setLogin, setToken) => {
+const Login = async (login, setLogin, toggleLoading, setToken, initialState) => {
   const { username, password } = login;
   if (!username.trim() || !password.trim()) {
     return setLogin({
@@ -9,13 +9,19 @@ const Login = async (login, setLogin, setToken) => {
       passwordError: !password.trim(),
     });
   }
+  toggleLoading(true);
   try {
     const { data } = await axios.post("/api/login", login);
     const token = data.data.accessToken;
+    if (!token) {
+      throw new Error("Invalid token");
+    }
     setToken(token);
+    setLogin(initialState);
   } catch (err) {
     alert(err.response?.data.message || err.message || err);
   }
+  toggleLoading(false);
 };
 
 export { Login };
