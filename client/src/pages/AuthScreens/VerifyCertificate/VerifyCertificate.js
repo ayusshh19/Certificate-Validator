2238f470097e60b7764dac29d55e4f36751897dd
Karan2310 <karandeep9619@gmail.com>
2024-02-14 01:29:46 +0530

    Certificate template optimized

--- client/src/pages/AuthScreens/VerifyCertificate/VerifyCertificate.js
+++ client/src/pages/AuthScreens/VerifyCertificate/VerifyCertificate.js
@@ -14,18 +14,62 @@ const VerifyCertificate = () => {
     AuthCertificate(id, toggleLoading, setCertificateData);
   }, []);
 
+  const formatDate = (dateString) => {
+    const date = new Date(dateString);
+    return date.toLocaleDateString("en-GB", {
+      day: "2-digit",
+      month: "long",
+      year: "numeric",
+    });
+  };
+
+  const getPositionText = (position) => {
+    if (position === 0) {
+      return "participation";
+    } else if (position === 1) {
+      return "1st";
+    } else if (position === 2) {
+      return "2nd";
+    } else if (position === 3) {
+      return "3rd";
+    } else {
+      return ``;
+    }
+  };
+
   return (
     <div className="auth-certi">
-      <div className="certificate">
-        <img src={Certificate} alt="" className="img-fluid" />
-        <p id="name">{certificateData?.name}</p>
-        <p id="desc">
-          has achieved the 1st position at the ABC Event organized by CSI DMCE
-          at Datta Meghe College of Engineering.
-        </p>
-        {/* <p>Position: {certificateData?.position}</p>
-        <p>Date: {certificateData?.date}</p> */}
-      </div>
+      {certificateData && (
+        <div className="certificate">
+          <img src={Certificate} alt="" className="img-fluid" />
+          <p
+            id="title"
+            style={{
+              fontSize: `min(4vw, 45px)`,
+            }}
+          >
+            Certificate of{" "}
+            {certificateData?.position === 0 ? "Participation" : "Achievement"}
+          </p>
+          <p id="name">{certificateData?.name}</p>
+          {certificateData?.position === 0 ? (
+            <p id="desc">
+              for participation in the <b>{certificateData?.event_id}</b>{" "}
+              organized by CSI DMCE on {formatDate(certificateData?.date)}, at
+              Datta Meghe College of Engineering, Airoli.
+            </p>
+          ) : (
+            <p id="desc">
+              for achieving the{" "}
+              <b>{getPositionText(certificateData?.position)} position</b> at{" "}
+              <b>{certificateData?.event_id}</b> organized by CSI DMCE on{" "}
+              {formatDate(certificateData?.date)}, at Datta Meghe College of
+              Engineering, Airoli.
+            </p>
+          )}
+          <p id="uid">Certificate ID : {certificateData?.uid}</p>
+        </div>
+      )}
     </div>
   );
 };
